85d54f9af007f7c05edefa6da0feaf530328bbff
shohjahon077 <shoakem077@gmail.com>
2025-08-09 23:40:32 +0500

    ferka

--- script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const intro = document.getElementById("intro");
-  const cardsContainer = document.getElementById("cards-container");
-
-  setTimeout(() => {
-    intro.style.display = "none";          // Intro yo'qoladi
-    cardsContainer.classList.remove("hidden");  // Kartalar ko'rinadi
-  }, 15000); // 15 soniya davomida intro ko'rinadi
-});
